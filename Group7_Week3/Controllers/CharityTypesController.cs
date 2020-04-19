4629b98d5455aeb61203ae99bc44428502cc9058
Prit Patel <prit.patel1104@gmail.com>
2020-04-19 19:34:57 -0400

    Doantion pages added

---
+++ Group7_Week3/Controllers/CharityTypesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Group7_Week3.Models;
+
+namespace Group7_Week3.Controllers
+{
+    public class CharityTypes : Controller
+    {
+        public IActionResult Animals()
+        {
+            return View();
+        }
+        public IActionResult Medical()
+        {
+            return View();
+        }
+        public IActionResult Memorial()
+        {
+            return View();
+
+        }
+        public IActionResult Education()
+        {
+            return View();
+
+        }
+        public IActionResult FoodBank()
+        {
+            return View();
+
+        }
+        public IActionResult CancerPatient()
+        {
+            return View();
+
+        }
+    }
+}
